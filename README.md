3eaadaf7e562b68870656496db51badaceb824b2
anjan <anjangantapara>
2021-04-17 13:52:18 +0200

    updated readme and app file

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # mlops-flask-search
 
 flask application to store a document and search for it later
+
+
+Command to send json file:
+echo '{"doc0": "new text up"}' | http POST http://localhost:8000/api/doc
+
+command to call the doc names
+ http -v GET http://localhost:8000/api/print_doc_names
+
--- src/flasksearchapp/app.py
+++ src/flasksearchapp/app.py
@@ -1,9 +1,8 @@
 import os
 
-from flask import Flask
-from redis import Redis
+from flask import Flask, request
 from nltk.tokenize import word_tokenize
-
+from redis import Redis
 
 app = Flask(__name__)
 r = Redis(
@@ -15,9 +14,26 @@ r = Redis(
 REDIS_NAMES_KEY = "names"
 
 
-@app.route("/doc/<doc>", methods=["POST"])
+@app.route('/api/<doc>', methods=['POST'])
 def process_doc(doc):
-    tokens = word_tokenize(doc)
+    content = dict(request.json)
+    doc_name = list(content.keys())[0]
+    doc_content = list(content.values())[0]
+
+    tokens = word_tokenize(doc_content)
     for i in tokens:
-        new = r.rpush(i, "doc1")
+        new = r.rpush(i, doc_name)
     return {"tokens": list(tokens)}
+
+@app.route('/api/<print_doc_names>', methods=['GET'])
+def print_doc_names_api(print_doc_names):
+    return {"total docs": r.llen()}
+#
+# @app.route('/api/add_message/<uuid>', methods=['GET', 'POST'])
+# def add_message(uuid):
+#     content = request.json
+#     print content['mytext']
+#     return jsonify({"uuid":uuid})
+
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0',debug=True)
